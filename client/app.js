b7eaa84d56ad97f4c70890e0f07587ebb437a39e
mendez6412 <mendez6412@gmail.com>
2019-01-23 16:45:44 -0500

    started client side of map practice

---
+++ client/app.js
@@ -0,0 +1,15 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibWVuZGV6NjQxMiIsImEiOiJjanI4aXFsd3IwN2VkNDRwZG9sanUybWZkIn0.ZoqHstzqGbjETTzoDqWjeg';
+
+var map = new mapboxgl.Map({
+    container: 'heatmap',
+    center: [-78.8986, 35.9940],
+    zoom: 13,
+    style: 'mapbox://styles/mapbox/streets-v9',
+  });
+
+map.on('render', (e) => {
+    // TODO: Add debounce time here so it's not constantly updating boundaries
+    console.log('event: ', e);
+    const boundaries = e.target.getBounds();
+    console.log('bounds: ', boundaries._sw);
+})
